5ae4f280a5f2404f44ee31c42465331a306fd52d
AniketSamratMazumdar <aniket.smazumdar@gmail.com>
2016-07-06 19:32:58 +0530

    Update complete_code.ino

--- arduino1Code/complete_code.ino
+++ arduino1Code/complete_code.ino
@@ -22,30 +22,6 @@
 
 MS5837 sensor;
 
-Encoder EnR(2, 5);#define portOfPin(P)\
-  (((P)>=0&&(P)<8)?&PORTD:(((P)>7&&(P)<14)?&PORTB:&PORTC))
-#define ddrOfPin(P)\
-  (((P)>=0&&(P)<8)?&DDRD:(((P)>7&&(P)<14)?&DDRB:&DDRC))
-#define pinOfPin(P)\
-  (((P)>=0&&(P)<8)?&PIND:(((P)>7&&(P)<14)?&PINB:&PINC))
-#define pinIndex(P)((uint8_t)(P>13?P-14:P&7))
-#define pinMask(P)((uint8_t)(1<<pinIndex(P)))
-
-#define pinAsInput(P) *(ddrOfPin(P))&=~pinMask(P)
-#define pinAsInputPullUp(P) *(ddrOfPin(P))&=~pinMask(P);digitalHigh(P)
-#define pinAsOutput(P) *(ddrOfPin(P))|=pinMask(P)
-#define digitalLow(P) *(portOfPin(P))&=~pinMask(P)
-#define digitalHigh(P) *(portOfPin(P))|=pinMask(P)
-#define isHigh(P)((*(pinOfPin(P))& pinMask(P))>0)
-#define isLow(P)((*(pinOfPin(P))& pinMask(P))==0)
-#define digitalState(P)((uint8_t)isHigh(P))
-
-#include <Encoder.h>
-#include <Wire.h>
-#include "MS5837.h"
-
-MS5837 sensor;
-
 Encoder EnR(2, 5);
 Encoder EnL(3, 11);  
 
@@ -68,22 +44,25 @@ int inpen=0;   //target value for encoder received from serial read - assuming b
 int inp[6];
 int inpi=0;
 int count = 0;
-int CsValue=0;         //Current sensor
-float AcsValue = 0.0;  //Average current sensor value
-int analogInput = A1;  //Voltage sensor
-int VsValue = 0;       //Voltage sensor value
-float AvsValue = 0.0;  //Average voltage value
+
 int pres=0,temp=0;
 float Apres=0,Atemp=0;
 long output;           //Cs and Vs values sent to python PC
-int AnalogInPin=A0;  //Current sensor
+//int AnalogInPin=A0;  //Current sensor
+
+int differenceR = 0;
+int differenceL = 0;
+int differenceR_P = 0;
+int differenceL_P = 0;
+int direction_reversed_M1 = 0;
+int direction_reversed_M2 =0;
 
 void setup() { 
 
   Serial.begin(38400);
   
-  pinMode(analogInput, INPUT);
-  pinMode(AnalogInPin,INPUT);
+//  pinMode(analogInput, INPUT);
+//  pinMode(AnalogInPin,INPUT);
 
   Serial.println("Starting");
   Wire.begin();
@@ -106,6 +85,8 @@ void setup() {
   analogWrite(M2PWM,0);
 } 
 
+
+
 void loop(){
 
   long EnR_Pos = EnR.read();
@@ -113,14 +94,14 @@ void loop(){
   
   //VOLTAGE SENSOR    
   // read the value at analog input
-  VsValue = analogRead(analogInput);
+  //VsValue = analogRead(analogInput);
 
   
   //CURRENT SENSOR
   //Read current sensor values   
-  CsValue = analogRead(AnalogInPin);  
-  AcsValue = AcsValue + CsValue;
-  AvsValue = AvsValue + VsValue;
+  //CsValue = analogRead(AnalogInPin);  
+//  AcsValue = AcsValue + CsValue;
+//  AvsValue = AvsValue + VsValue;
 
   //DEPTH SENSOR
   sensor.read();
@@ -160,64 +141,57 @@ void loop(){
   }
 
 //ENCODER MOTOR LEFT
-  while((abs(EnR_Pos) - abs(inpen)) || (abs(EnL_Pos) - abs(inpen)))
-  {
-   // Serial.println(EnR_Pos);
-   // Serial.println(inpen);
-    digitalLow(M1BRK);
-    digitalLow(M1BRK);
-    analogWrite(M1PWM,100);
-    analogWrite(M2PWM,100);
-      
-    long EnL_Pos = EnL.read();
-    long EnR_Pos = EnR.read();
-
-          
-    if (EnR_Pos < inpen )
+  if (inpen > 0 && inpen < 1530)
+  { analogWrite(M1PWM,100);
+    analogWrite(M2PWM,100); 
+    while(1)
+   {
+    long EnL_Pos=EnL.read();
+    long EnR_Pos=EnR.read();
+    if (abs(EnL_Pos)<inpen)
     {
       digitalLow(M1BRK);
-      digitalHigh(M1Dir);   //change according to direction
-      //Serial.print("Enr less than inpen. M1Dir is High");
-      //Serial.print(EnR_Pos);
-      //Serial.print("\t");
-      //Serial.println(inpen);
+      digitalLow(M1Dir);
       
     }
-    else if (EnR_Pos > inpen)
+    else if (abs(EnL_Pos)>inpen)
     {
       digitalLow(M1BRK);
-      digitalLow(M1Dir);  //change according to direction
-//      Serial.print("Enr higher than inpen. M1Dir is Low");
-//      Serial.print(EnR_Pos);
-//      Serial.print("\t");
-//      Serial.println(inpen);
-//      
+      digitalHigh(M1Dir);
+      
+    }
+    else if (abs(EnL_Pos)==inpen)
+    {
+      digitalHigh(M1BRK);
+    }
+
+    if (abs(EnR_Pos)<inpen)
+    { digitalLow(M2BRK);
+      digitalHigh(M2Dir);
+      
+    }
+    else if (abs(EnR_Pos)>inpen)
+    { digitalLow(M2BRK);
+      digitalLow(M2Dir);
+      
+    }
+    else if (abs(EnR_Pos)==inpen)
+    {
+      digitalHigh(M2BRK);
     }
     
-    if (EnL_Pos < inpen )
+    if ((abs(EnL_Pos)==inpen)&&(abs(EnR_Pos)==inpen))
     {
-      digitalLow(M2BRK);
-      digitalLow(M2Dir);   //Change direction - opp to M1
-//      Serial.print("Enl less than inpen. M1Dir is High");
-//      Serial.print(EnL_Pos);
-//      Serial.print("\t");
-//      Serial.println(inpen);
-      }
-    else if (EnL_Pos > inpen)
-    {
-      digitalLow(M2BRK);
-      digitalHigh(M2Dir);   //Change direction - opp to M2
-//      Serial.print("Enl less than inpen. M1Dir is High");
-//      Serial.print(EnL_Pos);
-//      Serial.print("\t");
-//      Serial.println(inpen);
-      }
+      break;
+    }
+   }
+   
     if (abs(EnR_Pos)>=1600)
       {
         analogWrite(M1PWM,0);
         digitalHigh(M1BRK);
         //Serial.println("Enr is 1600. Break applied");
-        break;
+        
       }
 
     if(abs(EnL_Pos)>=1600)
@@ -225,7 +199,7 @@ void loop(){
         analogWrite(M2PWM,0);
         digitalHigh(M2BRK);
         //Serial.println("Enl is 1600. Break applied");
-        break;
+        
       }
    
    
@@ -257,15 +231,15 @@ void loop(){
   if (count == 50)
   {
     count = 0;
-    AcsValue = AcsValue/50;
-    AvsValue = AvsValue/50;
+//    AcsValue = AcsValue/50;
+//    AvsValue = AvsValue/50;
     Apres=Apres/50;
     Atemp=Atemp/50;
     
-    Serial.print(AcsValue);
-    Serial.print("\t");
-    Serial.print(AvsValue);
-    Serial.print("\t");
+//    Serial.print(AcsValue);
+//    Serial.print("\t");
+//    Serial.print(AvsValue);
+//    Serial.print("\t");
     Serial.print(Apres);
     Serial.print("\t");
     Serial.print(Atemp);
@@ -275,194 +249,8 @@ void loop(){
     Serial.println(EnR_Pos);
     
         
-    AcsValue = 0;
-    AvsValue = 0;
-    Atemp=0;
-    Apres=0; 
-   }
-  }   
-  
-
-Encoder EnL(3, 11);  
-
-int M1BRK = 7;
-int M1Dir = 8;
-int M1PWM = 9;
-
-int M2BRK = 4;
-int M2Dir = 13;
-int M2PWM = 6;
-
-int TAILPWM=10;
-int TAILBRK=12;
-int TAILDIR=A7;
-
-int inptail=0;  //input speed of tail from serial read
-int inpen=0;   //target value for encoder received from serial read - assuming both encoders are same angles
-
-int inp;
-int count = 0;
-int CsValue=0;         //Current sensor
-float AcsValue = 0.0;  //Average current sensor value
-int analogInput = A1;  //Voltage sensor
-int VsValue = 0;       //Voltage sensor value
-float AvsValue = 0.0;  //Average voltage value
-int pres=0,temp=0;
-float Apres=0,Atemp=0;
-long output;           //Cs and Vs values sent to python PC
-int AnalogInPin=A0;  //Current sensor
-
-void setup() { 
-
-  Serial.begin(38400);
-  
-  pinMode(analogInput, INPUT);
-  pinMode(AnalogInPin,INPUT);
-
-  Serial.println("Starting");
-  Wire.begin();
-  sensor.init();
-  sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
-
-  pinMode(M1BRK,OUTPUT);
-  pinMode(M1Dir,OUTPUT);
-  pinMode(M1PWM,OUTPUT);
-  
-  pinMode(M2BRK,OUTPUT);
-  pinMode(M2Dir,OUTPUT);
-  pinMode(M2PWM,OUTPUT);
-  
-  pinMode(TAILBRK,OUTPUT);
-  pinMode(TAILDIR,OUTPUT);
-  pinMode(TAILPWM,OUTPUT);
-  
-  analogWrite(M1PWM,0);
-  analogWrite(M2PWM,0);
-} 
-
-void loop(){
-
-  long EnR_Pos = EnR.read();
-  long EnL_Pos = EnL.read();
-  
-  //VOLTAGE SENSOR    
-  // read the value at analog input
-  VsValue = analogRead(analogInput);
-
-  
-  //CURRENT SENSOR
-  //Read current sensor values   
-  CsValue = analogRead(AnalogInPin);  
-  AcsValue = AcsValue + CsValue;
-  AvsValue = AvsValue + VsValue;
-
-  //DEPTH SENSOR
-  sensor.read();
- 
-   
-  pres=sensor.pressure();
-  Apres=Apres+pres;
-   
-  temp=sensor.temperature(); 
-  Atemp=Atemp+temp;
-  
-  //SERIAL INPUT
-  if (Serial.available())  
-  {
-       inp = Serial.read();   // first digit for tail motor
-       inp = inp - '0' ;      // next three digits for en
-       inptail=inp/10000;
-       inp=inp%10000;
-       inpen=inp;
-   
-
-  //TAIL MOTOR
-  inptail = inptail * 28; //28*9  = 252  -- change factor if required
-  if (inptail>0)
-  {
-    digitalHigh(TAILDIR);
-    digitalLow(TAILBRK);
-    analogWrite(TAILPWM,inptail);
-  }
-
-//ENCODER MOTOR LEFT
-  while(abs(EnR_Pos) - abs(inpen))
-  {
-      analogWrite(M1PWM,100);
-      
-
-    if (EnR_Pos < inpen )
-    {
-      digitalLow(M1BRK);
-      digitalLow(M1Dir);  //change according to direction
-    }
-    else if (EnR_Pos > inpen)
-    {
-      digitalLow(M1BRK);
-      digitalHigh(M1Dir);  //change according to direction
-    }
-    long EnR_Pos = EnR.read();
-   if (abs(EnR_Pos)>=1600)
-   {
-    analogWrite(M1PWM,0);
-    break;
-    }
-   }
-   digitalHigh(M1BRK);
-
-  
-//ENCODER MOTOR RIGHT
-//digitalWrite(A2, HIGH);       // turn on pullup resistor
-
-  while(abs(EnL_Pos) - abs(inpen))
-  { 
-    analogWrite(M2PWM,100);
-    if (EnL_Pos < inpen )
-    {
-      digitalLow(M2BRK);
-      digitalHigh(M2Dir);   //Change direction - opp to M1
-    }
-    else if (EnL_Pos > inpen)
-    {
-      digitalLow(M2BRK);
-      digitalLow(M2Dir);   //Change direction - opp to M2
-    }
-    
-    long EnL_Pos = EnL.read();
-    if(abs(EnL_Pos)>=1600)
-    {
-      analogWrite(M2PWM,0);
-      break;
-      }
-   }
-   digitalHigh(M2BRK);
-  } 
-  
-   // send serial data to PC - current sensor and voltage sensor
-  count = count + 1;
-  if (count == 50)
-  {
-    count = 0;
-    AcsValue = AcsValue/50;
-    AvsValue = AvsValue/50;
-    Apres=Apres/50;
-    Atemp=Atemp/50;
-    
-    Serial.print(AcsValue);
-    Serial.print("\t");
-    Serial.print(AvsValue);
-    Serial.print("\t");
-    Serial.print(Apres);
-    Serial.print("\t");
-    Serial.print(Atemp);
-    Serial.print("\t");
-    Serial.print(EnL_Pos);
-    Serial.print("\t");
-    Serial.println(EnR_Pos);
-    
-        
-    AcsValue = 0;
-    AvsValue = 0;
+//    AcsValue = 0;
+//    AvsValue = 0;
     Atemp=0;
     Apres=0; 
    }
